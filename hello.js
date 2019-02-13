4218e8903d3c26ad65b7e45dc6e95aec36aec2bc
Anna Fatsevych <anna.fatsevych@flightnetwork.com>
2019-02-13 16:27:53 -0500

    added bootstrap

--- hello.js
+++ hello.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function hello() {
    alert("Hello, World");
 };
@@ -6,3 +7,7 @@ function hello() {
 function changeStyle() {
     document.getElementById('myButton').setAttribute("style", "background:deeppink; border: 1px solid purple;");   
 };
+
+function add(x = 5,y = 5) {
+   return x + y;
+}
