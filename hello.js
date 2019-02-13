a371ff229536e6c0ec3c427e07e3df2119431db0
Anna Fatsevych <anna.fatsevych@flightnetwork.com>
2019-02-13 11:50:44 -0500

    added more files

--- hello.js
+++ hello.js
@@ -1,3 +1,10 @@
+'use strict';
 function hello() {
    alert("Hello, World");
+};
+
+function changeStyle() {
+    document.getElementById('myButton').setAttribute("class", "buttonStyle");
+    console.log('"HERE!!!!!!');
+    
 };
